227370f92d900a28a4b281cc6396508a6cf00070
bugbit <ohernandezba@gmail.com>
2023-06-21 19:08:14 +0200

    voice

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using BlazorTableOfMultiply;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddSpeechSynthesis();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
