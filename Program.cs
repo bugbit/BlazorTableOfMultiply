8316bb8b286b125463ab75aa5f20dfbaac491a2b
bugbit <ohernandezba@gmail.com>
2023-07-03 18:28:20 +0200

    tabla

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-using Blazored.SessionStorage;
 using BlazorTableOfMultiply;
-using BlazorTableOfMultiply.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
@@ -11,11 +9,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // Third-party services
-builder.Services.AddBlazoredSessionStorage();
 builder.Services.AddSpeechSynthesis();
 
-builder.Services.AddScoped<CurrentVoiceOptions>();
-builder.Services.AddScoped<VoicesService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
--- Services/VoiceOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BlazorTableOfMultiply.Services;
-
-public class VoiceOptions
-{
-    public string? VoiceIdentity { get; set; }
-    public bool Speak { get; set; } = true;
-}
