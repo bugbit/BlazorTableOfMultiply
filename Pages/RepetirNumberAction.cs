6b79f905755b8778c78726fe29e1f7e48eb39831
bugbit <ohernandezba@gmail.com>
2023-07-06 16:19:42 +0200

    Boton repetir una línea

---
+++ Pages/RepetirNumberAction.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorTableOfMultiply.Pages;
+
+public class RepetirNumberAction
+{
+    private readonly Tabla _tabla;
+    private readonly int _m;
+
+    public RepetirNumberAction(Tabla tabla, int m)
+    {
+        _tabla = tabla;
+        _m = m;
+    }
+
+    public async Task DoAction() => await _tabla.DoRepetirNumber(_m);
+}
